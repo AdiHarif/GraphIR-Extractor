Ortal1993/compilation-project

Distinguishing between data and control edges/vertices

sources/graph.ts
@@ -26,11 +26,11 @@ export class Graph {
         }
     }
 
-    public addEdge(srcId: NodeId, dstId: NodeId, type: string): void {
+    public addEdge(srcId: NodeId, dstId: NodeId, kind: EdgeKind, label: string): void {
         this.checkVertexId(srcId);
         this.checkVertexId(dstId);
 
-        let newEdge: Edge = new Edge(srcId, dstId, type);
+        let newEdge: Edge = new Edge(srcId, dstId, kind, label);
         this.edges.push(newEdge);
     }
 
@@ -105,7 +105,7 @@ export class Graph {
     public print(humanFormat: boolean = false, filename: string | null = null): void {
         let content: string = "";
         if (humanFormat) {
-            this.edges.forEach(edge => {content += `source: ${edge.srcId}, dest: ${edge.dstId}, type: ${edge.type}`});
+            this.edges.forEach(edge => {content += `source: ${edge.srcId}, dest: ${edge.dstId}, type: ${edge.label}`});
             this.vertices.forEach(vertex => {content += `id: ${vertex.id}`});
         }
         else {
@@ -114,7 +114,7 @@ export class Graph {
                 content += `\t${vertex.id} [ label="${vertex.getLabel()}" shape="rectangle" ];\n`
             });
             this.edges.forEach(edge => {
-                content += `\t${edge.srcId} -> ${edge.dstId} [ label="${edge.type}" ];\n`
+                content += `\t${edge.srcId} -> ${edge.dstId} [ label="${edge.label}" ];\n`
             });
             content += "}\n";
         }
@@ -132,14 +132,16 @@ export class Graph {
     }
 }
 
-export class Edge {
-    public srcId: NodeId;
-    public dstId: NodeId;
-    public type: string;
+export enum EdgeKind {
+    Control,
+    Data
+};
 
-    public constructor(_srcId: NodeId, _dstId: NodeId, _type: string) {
-        this.srcId = _srcId;
-        this.dstId = _dstId;
-        this.type = _type;
-    }
+export class Edge {
+    public constructor(
+        public srcId: NodeId,
+        public dstId: NodeId,
+        public kind: EdgeKind,
+        public label: string
+    ) {};
 }

AdiHarif/GraphIR-Extractor

sources/graph.ts
@@ -3,9 +3,10 @@ import * as vertex from "./vertex";
 
 
 export enum EdgeType {
-    Standard,
+    Control,
+    Data,
     Association
-}
+};
 
 export class Graph {
     private edges: Array<Edge>;
@@ -18,7 +19,8 @@ export class Graph {
 
     private static typeToStyle(type: EdgeType, label: string) {
         switch(type) {
-            case EdgeType.Standard:
+            case EdgeType.Control:
+            case EdgeType.Data:
                 return `label="${label}"`;
             case EdgeType.Association:
                 return `label="${label}", style=dashed, dir=none`;
@@ -43,7 +45,7 @@ export class Graph {
         }
     }
 
-    public addEdge(srcId: NodeId, dstId: NodeId, label: string, type: EdgeType = EdgeType.Standard): void {
+    public addEdge(srcId: NodeId, dstId: NodeId, label: string, type: EdgeType): void {
         this.checkVertexId(srcId);
         this.checkVertexId(dstId);
 
@@ -125,7 +127,7 @@ export class Graph {
     public print(humanFormat: boolean = false, filename: string | null = null): void {
         let content: string = "";
         if (humanFormat) {
-            this.edges.forEach(edge => {content += `source: ${edge.srcId}, dest: ${edge.dstId}, type: ${edge.type}`});
+            this.edges.forEach(edge => {content += `source: ${edge.srcId}, dest: ${edge.dstId}, type: ${edge.label}`});
             this.vertices.forEach(vertex => {content += `id: ${vertex.id}`});
         }
         else {
@@ -152,17 +154,11 @@ export class Graph {
     }
 }
 
-
 export class Edge {
-    public srcId: NodeId;
-    public dstId: NodeId;
-    public label: string;
-    public type: EdgeType;
-
-    public constructor(_srcId: NodeId, _dstId: NodeId, _label: string, _type: EdgeType) {
-        this.srcId = _srcId;
-        this.dstId = _dstId;
-        this.label = _label;
-        this.type = _type;
-    }
+    public constructor(
+        public srcId: NodeId,
+        public dstId: NodeId,
+        public label: string,
+        public type: EdgeType
+    ) {};
 }