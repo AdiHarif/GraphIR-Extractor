47d38946c6c5e780f6fb5cc08e2e4fed774d2469
Adi Harif <adi.harif@campus.technion.ac.il>
2023-06-17 18:14:12 +0300

    Fixed database output

--- src/output/relations.ts
+++ src/output/relations.ts
@@ -4,18 +4,23 @@ import * as csv_writer from 'csv-writer'
 import * as ir from 'graphir'
 
 export async function exportIrToRelations(graph: ir.Graph, dir: string) {
-    const promises: Array<Promise<void>> = [];
-
     const verticesPath = `${dir}/vertices.facts`
     const verticesWriter = csv_writer.createArrayCsvWriter({path: verticesPath})
     const edgesPath = `${dir}/edges.facts`
     const edgesWriter = csv_writer.createArrayCsvWriter({path: edgesPath})
 
+    const vertices = [];
+    const edges = [];
+
     graph.vertices.forEach(v => {
-        promises.push(verticesWriter.writeRecords(graph.vertices.map(v => [ v.id, v.kind, v.category, v.label ])));
-        promises.push(edgesWriter.writeRecords(v.outEdges.map(e=> [ e.source.id, e.target.id, e.category, e.label ])));
+        vertices.push([ v.id, v.kind, v.category, v.label ]);
+        edges.push(...v.outEdges.map(e=> [ e.source.id, e.target.id, e.category, e.label ]));
     })
+    await Promise.all([
+        await verticesWriter.writeRecords(vertices),
+        await edgesWriter.writeRecords(edges)
+    ]);
 
 
-    await Promise.all(promises);
+
 }
