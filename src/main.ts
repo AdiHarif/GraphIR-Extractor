26385df717c453011d66946fb484419b36276b70
Adi Harif <adi.harif@campus.technion.ac.il>
2023-07-12 14:34:03 +0300

    Added extractFromPath API

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,15 @@ import { exportIrToDot } from "./output/dot";
 import { exportIrToRelations } from "./output/relations";
 import * as ast from './ts-ast'
 
+export function extractFromPath(path: string): ir.Graph {
+    const sourceFile = ast.parseFile(path)
+    return processSourceFile(sourceFile);
+}
+
 function main() {
     const outDir: string = process.argv[2]
     const sourceName: string = process.argv[3];
-
-    const sourceFile = ast.parseFile(sourceName)
-    const ir: ir.Graph = processSourceFile(sourceFile);
+    const ir: ir.Graph = extractFromPath(sourceName);
     exportIrToRelations(ir, outDir)
     exportIrToDot(ir, outDir)
 }
